00d22bc7215a37177b219892e0a92d7e465c1bd4
Aleksey Kruglik <kruglik@office.targetprocess.com>
2018-06-25 11:59:18 +0300

    Slack

--- background.js
+++ background.js
@@ -1,6 +1,51 @@
-// https://plan.tpondemand.com/entity/182918-email-notifications-backend-project-member-recipients
+// https://slack-redir.net/link?url=https%3A%2F%2Fplan.tpondemand.com%2Fentity%2F182918-email-notifications-backend-project-member-recipients
+chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
+    var re = /.*slack-redir\.net\/link\?url=.*%3A%2F%2F(\w*)\.tpondemand\.com%2Fentity%2F(\d*)/;
+    var match = e.url.match(re);
+    if (!match) {
+        console.log('no match, return');
+        return;
+    }
+    var account = match[1];
+    var entityId = match[2];
+    console.log(account, entityId);
+    if (!account || !entityId) {
+        console.log('Smth missing, return');
+        return;
+    }
 
-chrome.webNavigation.onCommitted.addListener(function(e) {
+    chrome.tabs.query({url: `*://${account}.tpondemand.com/*`}, function(tabs) {
+        tabs = tabs.filter(t => t.id !== e.tabId);
+        if (tabs.length === 0) {
+            console.log('No tabs, do nothing');
+            return;
+        }
+        if (tabs.length > 1) {
+            console.log('Several tabs, do nothing');
+            return;
+        }
+
+        chrome.tabs.sendMessage(tabs[0].id, {action: "openEntity", entityId}, function(response) {
+            if (!response) {
+                console.log('No response from tab');
+                return;
+            }
+            if (response.ok) {
+                console.log('Closing just opened tab');
+                chrome.tabs.remove(e.tabId);
+
+                console.log('Activating existing tab');
+                chrome.tabs.update(tabs[0].id, {active: true});
+            } else {
+                console.log('Not ot response from tab');
+            }
+        });
+    });
+}, {url: [{hostContains: 'slack-redir', queryContains: 'tpondemand.com'}]});
+
+
+// https://plan.tpondemand.com/entity/182918-email-notifications-backend-project-member-recipients
+chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
     if (e.transitionType === 'forward_back') {
         console.log('forward_back, return');
         return;
@@ -51,7 +96,7 @@ chrome.webNavigation.onCommitted.addListener(function(e) {
     });
 }, {url: [{hostContains: 'tpondemand.com', pathPrefix: '/restui/board.aspx'}]});
 
-chrome.webNavigation.onCommitted.addListener(function(e) {
+chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
     if (e.transitionType === 'forward_back') {
         console.log('forward_back, return');
         return;
@@ -104,8 +149,7 @@ chrome.webNavigation.onCommitted.addListener(function(e) {
 
 
 // http://localhost/targetprocess/entity/168-lorem-ipsum-dolor-sit-amet-consectetur
-
-chrome.webNavigation.onCommitted.addListener(function(e) {
+chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
     if (e.transitionType === 'forward_back') {
         console.log('forward_back, return');
         return;
--- contentScript.js
+++ contentScript.js
@@ -2,9 +2,24 @@ console.log('Intab opener attached');
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log('Incoming request', request);
-    if (request.action === 'openEntity' && request.entityId && request.entityType) {
-        window.location.hash = `page=${request.entityType}/${request.entityId}`;
-        sendResponse({ok: true});
+    if (request.action === 'openEntity' && request.entityId) {
+        if (request.entityType) {
+            window.location.hash = `page=${request.entityType}/${request.entityId}`;
+            sendResponse({ok: true});
+            return;
+        }
+
+        fetch(window.location.origin + `/api/v1/generals/${request.entityId}?include=[entitytype[name]]&format=json`, {credentials: 'include'})
+            .then(r => r.text())
+            .then(r => JSON.parse(r))
+            .then(r => {
+                var entityType = r.EntityType.Name.toLowerCase();
+                window.location.hash = `page=${entityType}/${request.entityId}`;
+            })
+            .then(() => sendResponse({ok: true}))
+            .catch(() => sendResponse({ok: false}));
+
+        return true; // says that we're async
     } else {
         sendResponse({ok: false});
     }
