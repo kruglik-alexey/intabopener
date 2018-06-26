f6e8d58ccd245c9b05b4a81222ce841174a1415a
Aleksey Kruglik <kruglik@office.targetprocess.com>
2018-06-26 15:03:19 +0300

    Refactor

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-// TODO case insensivity
-
 function processResponse(response, existingTabId, newTabId) {
     if (!response) {
         console.log('No response from tab');
@@ -49,85 +47,36 @@ function sendMessageToAccountTab(account, message, newTabId) {
     }, () => {});
 }
 
+// Should handle direct urls as well as redirects of kind 'https://slack-redir.net/link?url=https%3A%2F%2Fplan.tpondemand.com%2Fentity%2F183866'
+var targetprocessEntityUrlRe = /.*:\/\/(\w*)\.tpondemand\.com\/entity\/(\d*)/;
+var targetprocessPageUrlRe = /.*:\/\/(\w*)\.tpondemand\.com\/restui\/board\.aspx.*page=(\w+?)\/(\d+)/;
+// TODO add support for [...]Board.aspx#page=board/4843129481926989378&appConfig=eyJhY2lkIjoiRTI2Njg5NzI5MkUxM0Q2ODIxOTBFOUFGODcxODRBQUUifQ==&boardPopup=userstory/169989/silent
+function tryParseTargetprocessEntityUrl(url) {
+    url = decodeURIComponent(url);
 
-// https://slack-redir.net/link?url=https%3A%2F%2Fplan.tpondemand.com%2Fentity%2F182918-email-notifications-backend-project-member-recipients
+    var match = url.match(targetprocessEntityUrlRe);
+    if (match) {
+        return {
+            account: match[1],
+            entityId: match[2]
+        };
+    }
+
+    match = url.match(targetprocessPageUrlRe);
+    if (match) {
+        return {
+            account: match[1],
+            entityType: match[2],
+            entityId: match[3]
+        };
+    }
+
+    return null;
+}
+
 chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
-    var re = /.*slack-redir\.net\/link\?url=.*%3A%2F%2F(\w*)\.tpondemand\.com%2Fentity%2F(\d*)/;
-    var match = e.url.match(re);
-    if (!match) {
-        console.log('no match, return');
-        return;
+    var entity = tryParseTargetprocessEntityUrl(e.url);
+    if (entity) {
+        sendMessageToAccountTab(entity.account, {action: "openEntity", entityId: entity.entityId, entityType: entity.entityType}, e.tabId);
     }
-    var account = match[1];
-    var entityId = match[2];
-    console.log(account, entityId);
-    if (!account || !entityId) {
-        console.log('Smth missing, return');
-        return;
-    }
-
-    sendMessageToAccountTab(account, {action: "openEntity", entityId}, e.tabId);
-}, {url: [{hostContains: 'slack-redir', queryContains: 'tpondemand.com%2Fentity%2F'}]});
-
-
-// https://www.google.com/url?hl=ru&q=https://plan.tpondemand.com/entity/183233&source=gmail&ust=1529999398532000&usg=AFQjCNEp-mq3ZyPxtRIxt7GEQnuSYmQAEA
-chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
-    var re = /.*google\..*\/url.*q=.*:\/\/(\w*)\.tpondemand\.com\/entity\/(\d*)/;
-    var match = e.url.match(re);
-    if (!match) {
-        console.log('no match, return');
-        return;
-    }
-    var account = match[1];
-    var entityId = match[2];
-    console.log(account, entityId);
-    if (!account || !entityId) {
-        console.log('Smth missing, return');
-        return;
-    }
-
-    sendMessageToAccountTab(account, {action: "openEntity", entityId}, e.tabId);
-}, {url: [{hostContains: 'google', queryContains: 'tpondemand.com/entity/'}]});
-
-
-// https://plan.tpondemand.com/entity/182291-assign-many-teams-to-epics-and
-chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
-    var re = /\w*:\/\/(\w*)\.tpondemand\.com\/entity\/(\d*)/;
-    var match = e.url.match(re);
-    if (!match) {
-        console.log('no match, return');
-        return;
-    }
-
-    var account = match[1];
-    var entityId = match[2];
-    console.log(account, entityId);
-    if (!account || !entityId) {
-        console.log('Smth missing, return');
-        return;
-    }
-
-    sendMessageToAccountTab(account, {action: "openEntity", entityId}, e.tabId);
-}, {url: [{hostContains: 'tpondemand.com', pathPrefix: '/entity'}]});
-
-
-// https://plan.tpondemand.com/restui/board.aspx#page=feature/182291
-chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
-    var re = /\w*:\/\/(\w*)\.tpondemand\.com\/restui\/board\.aspx.*page=(\w+?)\/(\d+)/;
-    var match = e.url.match(re);
-    if (!match) {
-        console.log('no match, return');
-        return;
-    }
-
-    var account = match[1];
-    var entityType = match[2];
-    var entityId = match[3];
-    console.log(account, entityType, entityId);
-    if (!account || !entityId || !entityType) {
-        console.log('Smth missing, return');
-        return;
-    }
-
-    sendMessageToAccountTab(account, {action: "openEntity", entityId, entityType}, e.tabId);
-}, {url: [{hostContains: 'tpondemand.com', pathPrefix: '/restui/board.aspx'}]});
+});
